367c0dd23dd2905ad7ad315265b0e97924a8218a
Shubham <nakumsky@gmail.com>
2022-09-09 22:24:15 +0530

    comitting

--- src/main/java/multithreading/threadgroup/ActiveThreadNamesOfSystemGroup.java
+++ src/main/java/multithreading/threadgroup/ActiveThreadNamesOfSystemGroup.java
@@ -6,7 +6,7 @@ public class ActiveThreadNamesOfSystemGroup {
         Thread[] t=new Thread[system.activeCount()];
         system.enumerate(t);
         for(Thread t1:t){
-            System.out.println(t1.getName()+"......."+t1.isDaemon());
+            System.out.println(t1.getName()+"......"+t1.isDaemon());
         }
     }
 }
